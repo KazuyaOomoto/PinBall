d5bb5f3b3a4a75547fecfeea089f69de2d6fe8ea
KazuyaOomoto <n4p4yeah@gmail.com>
2019-02-20 01:41:18 +0900

    PinBall初めてのコミット
    要件：得点機能の実装
    発展課題：スマートフォンでも動かせるようにマルチタッチに対応

---
+++ Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	/* 課題追加箇所	*/
+	//得点を表示するテキスト
+	private GameObject scoreText;
+	//得点保持用
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		/* 課題追加箇所	*/
+		this.scoreText = GameObject.Find ("ScoreText");
+		//得点を0で初期化
+		this.scoreText.GetComponent<Text> ().text = "SCORE:" + this.score.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameoverTextにゲームオーバを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+	}
+
+	/* 課題追加箇所	*/
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision col) {
+		//対象が大の星または雲の場合
+		if (col.gameObject.CompareTag ("LargeStarTag") || col.gameObject.CompareTag ("LargeCloudTag")) {
+			/*20点加算する	*/
+			this.score += 20;
+		}
+		//対象が小の星または雲の場合
+		else if (col.gameObject.CompareTag ("SmallStarTag") || col.gameObject.CompareTag ("SmallCloudTag")) {
+			/*10点加算する	*/
+			this.score += 10;
+		}
+		this.scoreText.GetComponent<Text> ().text = "SCORE:" + this.score.ToString();
+	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//左矢印キーを押した時左フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		//右矢印キーを押した時右フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		//矢印キー離された時フリッパーを元に戻す
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+		//タッチパネル追加処理
+		foreach (Touch t in Input.touches)
+		{
+			switch(t.phase)
+			{
+				case TouchPhase.Began:		/* 画面に指が触れたとき	*/
+					/* 画面左半分をタッチした場合は、左のフリッパーを弾く */
+					if((t.position.x >= 0) && (t.position.x <= Screen.width/2) && (tag=="LeftFripperTag"))
+							SetAngle (this.flickAngle);
+					/* 画面右半分をタッチした場合は、右のフリッパーを弾く */
+					if((t.position.x  > Screen.width/2) && (t.position.x  <= Screen.width) && (tag=="RightFripperTag"))
+							SetAngle (this.flickAngle);
+					break;
+				case TouchPhase.Ended:		/* 画面から指が離れたとき	*/
+				case TouchPhase.Canceled:	/* システムがタッチの追跡をキャンセル */
+					/* 画面左半分から離した場合は、左のフリッパーを戻す */
+					if((t.position.x  >= 0) && (t.position.x  <= Screen.width/2) && (tag=="LeftFripperTag"))
+							SetAngle (this.defaultAngle);
+					/* 画面右半分から離した場合は、右のフリッパーを戻す */
+					if((t.position.x  > Screen.width/2) && (t.position.x  <= Screen.width) && (tag=="RightFripperTag"))
+							SetAngle (this.defaultAngle);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	//フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
